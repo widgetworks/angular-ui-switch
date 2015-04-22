a79f23b659d1c066a296588de366a2e8a3798d2f
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2015-04-22 14:39:33 +1000

    feat(switch): activate with keyboard; allow ng-disabled directive

--- angular-ui-switch.js
+++ angular-ui-switch.js
@@ -1,23 +1,60 @@
 angular.module('uiSwitch', [])
 
-.directive('switch', function(){
+.directive('switch', [
+  '$parse',
+function($parse){
   return {
     restrict: 'AE'
   , replace: true
   , transclude: true
+  , compile: function(tElement, tAttrs){
+    
+    var isDisabled = $parse(tAttrs.ngDisabled);
+    var modelGetter = $parse(tAttrs.ngModel);
+    
+    // Change handler.
+    var change = angular.noop;
+    if (tAttrs.ngChange){
+      change = $parse(tAttrs.ngChange);
+    }
+    
+    return function link($scope, element, attrs){
+        
+        $scope.toggleSwitch = function(event){
+            // Skip if we're disabled or key was not `space`.
+            var validKey = (event.keyCode == 32 /*32 == space*/ || 
+                            event.keyCode == 13 /*13 == enter*/);
+            
+            if (isDisabled($scope) || (event.keyCode && !validKey)){
+                return;
+            }
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            
+            // Invert.
+            var newValue = !modelGetter($scope);
+            modelGetter.assign($scope, newValue);
+            
+            // Raise change event.
+            change($scope);
+        }
+    }
+  }
   , template: function(element, attrs) {
       var html = '';
-      html += '<span';
-      html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
-      html +=   attrs.ngModel ? ' ng-click="' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"') : '';
+      html += '<span ng-attr-tabindex="{{'+attrs.ngDisabled+' ? undefined : 0}}"';
+      html +=   ' class="switch"';
+      html +=   ' ng-click="toggleSwitch($event)"';
+      html +=   ' ng-keydown="toggleSwitch($event)"';
       html +=   ' ng-class="{ checked:' + attrs.ngModel + ' }"';
       html +=   '>';
       html +=   '<small></small>';
       html +=   '<input type="checkbox"';
+      html +=     ' class="ui-helper-hidden-accessible"';
       html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
       html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
       html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
-      html +=     ' style="display:none" />';
+      html +=     ' />';
       html +=     '<span class="switch-text">'; /*adding new container for switch text*/
       html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; /*switch text on value set by user in directive html markup*/
       html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  /*switch text off value set by user in directive html markup*/
@@ -25,4 +62,4 @@ angular.module('uiSwitch', [])
       return html;
     }
   }
-});
+}]);
